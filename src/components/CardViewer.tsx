921d71a69506aeccc7d18cba6ccb94ed617899de
cthacker-udel <cthacker@udel.edu>
2021-09-23 20:46:21 -0400

    Added Card functionality to CardViewer

--- src/components/CardViewer.tsx
+++ src/components/CardViewer.tsx
@@ -1,7 +1,25 @@
-
+import {Card, Button} from 'react-bootstrap';
 
 export const CardViewer = (): JSX.Element => {
 
-    return <div>CardViewer</div>
+    return(
+
+        <>
+
+            <Card style={{width: "18px"}}>
+            
+                <Card.Body>
+
+                    <Card.Title>{""}</Card.Title>
+                    <Card.Text>{""}</Card.Text>
+                    <Button variant="primary" onClick={() => {console.log("hello");}}>Show Answer</Button>
+                </Card.Body>
+            
+            </Card>
+
+        </>
+
+
+    );
 
 }
